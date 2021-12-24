75e705d85f53b8b49005e7b02fc7d90c74b9239b
pramodpatwawork <pramod_patwa2003@yahoo.com>
2021-12-24 19:59:45 +0530

    cluster configuration

--- src/main/java/com/mywork/pp/spring/redis/integration/config/AppConfig.java
+++ src/main/java/com/mywork/pp/spring/redis/integration/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.mywork.pp.spring.redis.integration.config;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,10 +17,17 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 
 @Configuration
 @EnableCaching
@@ -27,40 +37,78 @@ public class AppConfig {
 
     private @Value("${redis.host}") String redisHost;
     private @Value("${redis.port}") int redisPort;
+    private @Value("${redis.nodes}") String redisNodes;
+    private @Value("${redis.maxRedirects}") int maxRedirects;
     private static final Map<String, RedisCacheConfiguration> redisConfigs = new HashMap<String, RedisCacheConfiguration>();
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
+    
+    @Bean(destroyMethod = "shutdown")
+    ClientResources clientResources() {
+        return DefaultClientResources.create();
+    }
 
-   /* 
     @Bean
-    public RedisConnectionFactory jedisConnectionFactory() {
-      RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-      .master("themaster")
-      .sentinel("127.0.0.1", 26579)
-      .sentinel("127.0.0.1", 26580);
-      return new JedisConnectionFactory(sentinelConfig);
-    }*/
-     
+    public RedisClusterConfiguration  redisClusterConfiguration() {
+    	
+    	 RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+
+         List<RedisNode> nodeList = new ArrayList<>();
+         
+         String[] nodePairs = redisNodes.split(",");
+         
+         for(String nodePair: nodePairs) {
+        	 String[] nodeHostPort = nodePair.split(":");
+        	 nodeList.add(new RedisNode(nodeHostPort[0], Integer.valueOf(nodeHostPort[1])));
+         }
+         
+         clusterConfig.setClusterNodes(nodeList);
+         clusterConfig.setMaxRedirects(maxRedirects);
+         //config.setPassword(RedisPassword.of(password));
+         return clusterConfig;
+         
+    }
+
+    @Bean
+    public ClientOptions clientOptions(){
+        return ClientOptions.builder()
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                .autoReconnect(true)
+                .build();
+    }
+    
+    @Bean
+    LettucePoolingClientConfiguration lettucePoolConfig(ClientOptions options, ClientResources dcr){
+    	GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+    	genericObjectPoolConfig.setMaxIdle(50);
+    	genericObjectPoolConfig.setMaxTotal(100);    	
+    	genericObjectPoolConfig.setMinIdle(10);
+    	
+    	
+    	return LettucePoolingClientConfiguration.builder()
+                .poolConfig(genericObjectPoolConfig)
+                .clientOptions(options)
+                .clientResources(dcr)
+                .build();
+    }    
+    
     /**
      * Lettuce
      */
     @Bean
-    public RedisConnectionFactory lettuceConnectionFactory() {
-      RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-      .master("mymaster")
-      .sentinel("127.0.0.1", 26379)
-      .sentinel("127.0.0.1", 26380)
-      .sentinel("127.0.0.1", 26381);
-      return new LettuceConnectionFactory(sentinelConfig);
+    public RedisConnectionFactory redisConnectionFactory(LettucePoolingClientConfiguration lettucePoolingClientConfiguration, 
+    					RedisClusterConfiguration  redisClusterConfiguration) {
+          	
+      return new LettuceConnectionFactory(redisClusterConfiguration, lettucePoolingClientConfiguration);
     }
 
     @Bean
-    RedisTemplate<Object, Object> redisTemplate() {
+    RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
-        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 
