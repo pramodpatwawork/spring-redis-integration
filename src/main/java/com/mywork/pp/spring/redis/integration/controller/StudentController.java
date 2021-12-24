0f004cb1a96b37538a3d58f0fc9b7d6fb14080ce
pramodpatwawork <pramod_patwa2003@yahoo.com>
2021-12-24 17:02:58 +0530

    add evict logic on update

--- src/main/java/com/mywork/pp/spring/redis/integration/controller/StudentController.java
+++ src/main/java/com/mywork/pp/spring/redis/integration/controller/StudentController.java
@@ -1,13 +1,16 @@
 package com.mywork.pp.spring.redis.integration.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mywork.pp.spring.redis.integration.model.Student;
 import com.mywork.pp.spring.redis.integration.service.StudentService;
-
 
 @RestController
 public class StudentController {
@@ -19,5 +22,13 @@ public class StudentController {
 	public Student findStudentById(@PathVariable String id) {
 		System.out.println("Searching by ID  : " + id);
 		return studentService.getStudent(id);
+	}
+	
+	@PutMapping("/students/{id}")
+	public ResponseEntity<Void> updateStudent(@PathVariable String id, @RequestBody Student student) {
+		System.out.println("Searching by ID  : " + id);
+		studentService.updateStudent(id, student);
+		return new ResponseEntity<Void>( HttpStatus.OK );
+
 	}
 }
--- src/main/java/com/mywork/pp/spring/redis/integration/service/StudentService.java
+++ src/main/java/com/mywork/pp/spring/redis/integration/service/StudentService.java
@@ -1,5 +1,8 @@
 package com.mywork.pp.spring.redis.integration.service;
 
+import java.security.SecureRandom;
+
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +11,23 @@ import com.mywork.pp.spring.redis.integration.model.Student;
 @Service
 public class StudentService {
 
-    @Cacheable(value="student", key="#studentId")
-    public Student getStudent( final String studentId ) {
-        try {
+	@Cacheable(value = "student", key = "#studentId")
+	public Student getStudent(final String studentId) {
+		try {
 			Thread.sleep(10000);
-		} catch (InterruptedException e) {
+
+			System.out.println("Executing: " + this.getClass().getSimpleName() + ".getStudent(" + studentId + ")");
+			return new Student(studentId, Integer.toString(SecureRandom.getInstanceStrong().nextInt()), "ABC");
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	System.out.println("Executing: " + this.getClass().getSimpleName() + ".getStudent(" + studentId + ")");
-        return new Student(studentId, "XYZ", "ABC");
-    }
+		return null;
+	}
+
+	@CacheEvict(value = "student", key = "#studentId")
+	public void updateStudent(String studentId, Student student) {
+		System.out.println("Updating data base so removed cache: " + this.getClass().getSimpleName() + ".updateStudent("
+				+ studentId + ")");
+	}
 }
